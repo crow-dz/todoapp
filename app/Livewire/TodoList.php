d04abf49cc3d8f2ddedde1c2a2be48a50863a4a8
crow-dz <walid.cena@gmail.com>
2025-03-16 08:27:31 +0100

    adding complete todo

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -8,48 +8,64 @@ use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class TodoList extends Component 
+
+class TodoList extends Component
 {
-    
+    use WithPagination;
+    protected $paginationTheme = 'tailwind';
     public $user;
     #[Rule('required|min:3')]
     public $content;
 
-    public $userid;  
-    
+    public $userid;
+
     public $search;
 
-     public function mount(){
-       $this->user= User::find(1);
-     }
+    public function mount()
+    {
+        $this->user = User::find(1);
+    }
 
- 
 
-     public function createTodo(){
 
-    $this->validateOnly('content');
+    public function createTodo()
+    {
+
+        $this->validateOnly('content');
         Note::create([
-            'content'=>$this->content,
-            'user_id'=>$this->userid,
-            'is_completed'=>false,
-            'create_at'=>now()
+            'content' => $this->content,
+            'user_id' => $this->userid,
+            'is_completed' => false,
+            'create_at' => now()
         ]);
-        request()->session()->flash("success","Created.");
+        request()->session()->flash("success", "Created.");
         $this->reset(['content']);
 
-      
-     }
-    public function deleteTodo(Note $todo){
-     $todo->delete();
     }
-    
+
+
+
+    public function deleteTodo(Note $todo)
+    {
+        $todo->delete();
+    }
+    public function toggleComplete($todoId)
+    {
+
+        $todo = Note::find($todoId);
+        $todo->is_completed = !$todo->is_completed;
+        $todo->save();
+    }
+
+
     public function render()
     {
-        $todos = Note::latest()->where('content','like',"%{$this->search}%")->paginate(5);
+        $todos = Note::latest()->where('content', 'like', "%{$this->search}%")->paginate(5);
 
-        return view('livewire.todo-list',[
-            'user'=>$this->user,
-            'todos'=>$todos
+        return view('livewire.todo-list', [
+            'user' => $this->user,
+            'todos' => $todos
         ]);
+
     }
 }
--- resources/views/livewire/includes/todo-card.blade.php
+++ resources/views/livewire/includes/todo-card.blade.php
@@ -1,4 +1,5 @@
-<div  wire:key="{{$todo->id}}" class="todo mb-5 card px-5 py-6 bg-white col-span-1 border-t-2 border-blue-500 hover:shadow">
+<div wire:key="{{ $todo->id }}"
+    class="todo mb-5 card px-5 py-6 bg-white col-span-1 border-t-2 border-blue-500 hover:shadow">
     <div class="flex justify-between space-x-2">
 
         <!-- <input type="text" placeholder="Todo.."
@@ -7,8 +8,19 @@
                     
                         <span class="text-red-500 text-xs block">error</span> -->
 
-        <h3 class="text-lg text-semibold text-gray-800">{{$todo->content}}</h3>
+        <div class="flex flex-row items-center space-x-2 justify-between ">
 
+            @if ($todo->is_completed)
+                <input type="checkbox" class=" rounded text-teal-500" wire:click="toggleComplete({{ $todo->id }})"
+                    checked>
+            @else
+                <input type="checkbox" class=" rounded text-teal-500" wire:click="toggleComplete({{ $todo->id }})">
+            @endif
+
+
+            <h3 class="text-lg text-semibold text-gray-800">{{ $todo->content }}</h3>
+
+        </div>
 
 
         <div class="flex items-center space-x-2">
@@ -19,7 +31,8 @@
                         d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
             </button>
-            <button wire:click='deleteTodo({{$todo->id}})' class="text-sm text-red-500 font-semibold rounded hover:text-teal-800 mr-1">
+            <button wire:click='deleteTodo({{ $todo->id }})'
+                class="text-sm text-red-500 font-semibold rounded hover:text-teal-800 mr-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -28,7 +41,7 @@
             </button>
         </div>
     </div>
-    <span class="text-xs text-gray-500"> {{$todo->created_at}} </span>
+    <span class="text-xs text-gray-500"> {{ $todo->created_at }} </span>
     <div class="mt-3 text-xs text-gray-700">
         <!--
                     <button
